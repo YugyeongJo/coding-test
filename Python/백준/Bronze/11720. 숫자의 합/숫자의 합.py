753d2ec23ced96ac44cd29d2b3dd953a25749801
조유경 <yugyeongjo1215@gmail.com>
2024-06-10 11:25:23 +0900

    [Bronze IV] Title: 숫자의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -0,0 +1,12 @@
+count = int(input())
+num = input()
+list_num = list(map(int, num))
+
+pass
+def num_sum(list_num):
+    result = sum(list_num)
+    return result
+
+answer = num_sum(list_num)
+print(answer)
+    
